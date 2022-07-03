4b9f2f710473936cd68769d447b5bb8bbf4b17ec
Anisha-C <99618409+Anisha-C@users.noreply.github.com>
2022-07-03 09:11:32 -0400

    Display current and future forecast

--- js/script.js
+++ js/script.js
@@ -15,3 +15,53 @@ function weatherApi(lat, lon, name) {
             displayFuture(info.daily)
         })
 }
+
+function displayCurrent(current, name) {
+    let icon = current.weather[0].icon
+    let title = document.getElementById("city-name")
+    let date = new Date(current.dt * 1000).toLocaleDateString()
+    let temperature = document.getElementById("temp")
+    let winds = document.getElementById("wind")
+    let hum = document.getElementById("humidity")
+    let ultv = document.getElementById("UV")
+    title.innerText = `${name} ${date}`
+    temperature.innerText = `Temp: ${current.temp} deg`
+    winds.innerText = `Wind Speed: ${current.wind_speed}`
+    hum.innerText = `Humitidity: ${current.humitidy}`
+    ultv.innerText = `UV: ${current.uvi}`
+    if (current.uvi < 4) {
+        ultv.style.backgroundColor = "lightblue"
+    }
+    else if (current.uvi >= 4 && current.uvi < 7) {
+        ultv.style.backgroundColor = "yellow"
+    }
+    else { ultv.style.backgroundColor = "red" }
+
+    document.getElementById("icon").src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+}
+function displayFuture(daily) {
+    let futureHtml = ""
+    for (let i = 1; i < 6; i++) {
+        let time = new Date(daily[i].dt * 1000).toDateString()
+        let icon = daily[i].weather[0].icon
+        let temperature = daily[i].temp.day
+        let wind = daily[i].wind_speed
+        let humidity = daily[i].humidity
+        let card = `
+        <div class="card" style="width: 18rem;">
+        <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="card-img-top" alt="${daily[i].weather[0].description}">
+        <div class="card-body">
+            <h5 class="card-title">${time}</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Temp; ${temperature}</li>
+            <li class="list-group-item">Wind; ${wind}</li>
+            <li class="list-group-item">Humidity; ${humidity}</li>
+        </ul>
+    </div>
+        `
+        futureHtml += card
+    }
+    document.getElementById("future").innerHTML = futureHtml
+
+}
